35fd0a9360fcd56fbd58cf7357ffeb4450484e13
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-02-15 20:17:28 -0600

    added route endpoints

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import express from "express";
+import data from './data/data.json';
 
 const app = express();
 const PORT = 5000;
 
+app.get('/', (req, res) => {
+  res.send(`a get request with / route on port ${PORT}`)
+});
+
+app.post('/newItem', (req, res) => {
+  res.send(`a post request with /newItem on port ${PORT}`)
+});
+
+app.put('/item', (req, res) => {
+  res.send(`a get request with /item on port ${PORT}`)
+});
+
+app.delete('/item', (req, res) => {
+  res.send(`a delete request with / route on port ${PORT}`)
+});
+
 app.listen(PORT, () => {
   console.log(`Your Server is running on port ${PORT}`);
+  console.log(data);
 });
