d0efceb5d2eae5745389ea5dcdc38be8d5cdcefe
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-02-16 04:01:48 -0600

    serve-favicon middleware

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Mockaroo: website to generate mock data
+Express Routing Documentation
+Express Middleware Documentation
+
+ npm install --save-dev @babel/core @babel/cli @babel/preset-env @babel/node
--- index.js
+++ index.js
@@ -1,23 +1,71 @@
 import express from "express";
+import favicon from 'serve-favicon';
+import path from 'path';
 import data from './data/data.json';
 
 const app = express();
 const PORT = 5000;
 
+// This is for the public folder on path /  
+app.use(express.static('public'));
+
+// method to use JSON
+app.use(express.json());
+
+// method to use Urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// This is for images folder on path images
+app.use('/images', express.static('images'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.get('/', (req, res) => {
-  res.send(`a get request with / route on port ${PORT}`)
+  // get the data first from database
+  res.json(data);
 });
 
+//JSON data
+//{"hello":"JSON is cool"}
+// URLEncoded data
+// hello=URLEncoded+is+cool
 app.post('/newItem', (req, res) => {
-  res.send(`a post request with /newItem on port ${PORT}`)
+  console.log(req.body);
+  res.send(req.body);
 });
 
-app.put('/item', (req, res) => {
-  res.send(`a get request with /item on port ${PORT}`)
+app.get('/item/:id', (req, res, next) => {
+  //This is the middleware that pulls the data
+  console.log(req.params.id);
+  let user = Number(req.params.id);
+  console.log(data[user]);
+  res.send(data[user]);
+  // middleware that uses the request object
+  console.log(`Request from: ${req.originalUrl}`);
+  console.log(`Request type: ${req.method}`);
+  // Everything above is middleware
+  next();
+}, (req, res) => {
+  console.log('Did you get the right data');
 });
 
-app.delete('/item', (req, res) => {
-  res.send(`a delete request with / route on port ${PORT}`)
+app.route('/item')
+  .get((req, res) => {
+    //res.download('images/rocket.jpg');
+    //res.redirect('http://www.linkedin.com');
+    //res.end();
+    res.send(`a get request with /item on port ${PORT}`)
+  })
+  .put((req, res) => {
+    res.send(`a put request with /newItem on port ${PORT}`)
+  })
+  .delete((req, res) => {
+    res.send(`a delete request with / route on port ${PORT}`)
+  });
+
+// Error handling function
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(`Red Alert:${err.stack}`);
 });
 
 app.listen(PORT, () => {
